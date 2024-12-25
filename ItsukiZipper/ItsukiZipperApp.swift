f7d316cf6c5d47f9198ce578479f800a9210e332
itsuki <itsuki.enjyoy@gmail.com>
2024-12-26 08:53:09 +0900

    Initial Commit

---
+++ ItsukiZipper/ItsukiZipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItsukiZipperApp.swift
+//  ItsukiZipper
+//
+//  Created by Itsuki on 2024/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ItsukiZipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
